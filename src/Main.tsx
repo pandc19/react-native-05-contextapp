f48c283d39253c812d9687c8a67a1e9eefb9d88f
Jonathan Pillajo <pandc19@outlook.com>
2024-08-15 19:21:37 -0500

    end section 9

---
+++ src/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {BottomTabNavigator} from './presentation/navigators/BottomTabNavigator';
+
+export const Main = () => {
+  return (
+    <NavigationContainer>
+      <BottomTabNavigator />
+    </NavigationContainer>
+  );
+};
---
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect} from 'react';
+import {Pressable, Text, View} from 'react-native';
+import {styles} from '../../../config';
+import {useCounterStore} from '../../store/counter-store';
+import {useNavigation} from '@react-navigation/native';
+
+export const SettingsScreen = () => {
+  const count = useCounterStore(state => state.count);
+  const incrementBy = useCounterStore(state => state.incrementBy);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: `Contador: ${count}`,
+    });
+  }, [count]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Count: {count}</Text>
+
+      <Pressable style={styles.primaryButton} onPress={() => incrementBy(1)}>
+        <Text>+1</Text>
+      </Pressable>
+      <Pressable style={styles.primaryButton} onPress={() => incrementBy(-1)}>
+        <Text>-1</Text>
+      </Pressable>
+    </View>
+  );
+};
